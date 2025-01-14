26176922643342cf43137812f32495d9b9f1ac3e
rawhacker780085 <153677901+rawhacker780085@users.noreply.github.com>
2025-01-15 05:05:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ load_dotenv()
 
 
 class Config:
-    API_ID = os.getenv('API_ID')
-    API_HASH = os.getenv('API_HASH')
-    BOT_TOKEN = os.getenv('BOT_TOKEN')
+    API_ID = os.getenv('28372387')
+    API_HASH = os.getenv('11edc85521fb2d8f75e751bb284507e1')
+    BOT_TOKEN = os.getenv('7947440196:AAHH1fSzRvwvh6OI_61pBDYC6JUjQthvCYc')
